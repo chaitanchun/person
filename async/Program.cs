d3f331b8bc29410d1bed9ca16b4a2fe3ef65a6ce
chaitanchun <chaitanchun@gmail.com>
2014-11-20 13:53:09 +0800

    async

---
+++ async/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace async
+{
+    class Program
+    {
+        static void AccessWeb()
+        {
+            
+        }
+
+        async Task<int> AccessWebAsync()
+        {
+            HttpClient client = new HttpClient();
+            Task<string> getStringTask = client.GetStringAsync("http://msdn.microsoft.com");
+            string urlcontent = await getStringTask;
+            return urlcontent.Length;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
