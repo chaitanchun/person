98a944b31c229b63dee52886a713a2c6813da6a5
chaitanchun <chaitanchun@gmail.com>
2015-02-05 18:24:25 +0800

    skd

---
+++ leetcode/LeetCode/55.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+bool canJump(int A[], int n) 
+{
+    int ret = false;
+    for (int i = 0; i < 0; i++)
+    {
+
+    }
+
+    return ret;
+
+}
+int main()
+{
+    return 0;
+}
