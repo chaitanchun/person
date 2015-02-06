ca08a1383fa1678b1dc08698e9f8cb56ef9659dc
chaitanchun <chaitanchun@gmail.com>
2015-02-06 18:23:33 +0800

    sdlfkj

---
+++ leetcode/LeetCode/60.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string getPermutation(int n, int k)
+    {
+        vector<bool> selected(n, false);
+
+
+    }
+private:
+
+    vector
+};
+
+
+
+
+
+int main()
+{
+    return 0;
+}
