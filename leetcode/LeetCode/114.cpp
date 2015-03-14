7eef11d2128b87b29dffe48b91d5a5cd7d3885ed
chaitanchun <chaitanchun@gmail.com>
2015-03-15 01:00:24 +0800

    sldk

--- leetcode/LeetCode/114.cpp
+++ leetcode/LeetCode/114.cpp
@@ -11,7 +11,31 @@ class Solution {
 public:
     void flatten(TreeNode *root) 
     {
-
+        if (root == NULL)
+            return;
+        TreeNode* end;
+        dfs(root, end);
+    }
+private:
+    void dfs(TreeNode* root, TreeNode* &end)
+    {
+        end = root;
+        TreeNode* tempRight = root->right;
+        if (root->left != NULL)
+        {
+            TreeNode* leftEnd;
+            dfs(root->left, leftEnd);
+            root->right = root->left;
+            end = leftEnd;
+            root->left = NULL;
+        }
+        if (tempRight != NULL)
+        {
+            TreeNode* rightEnd;
+            dfs(tempRight, rightEnd);
+            end->right = tempRight;
+            end = rightEnd;
+        }
     }
 };
 int main()
